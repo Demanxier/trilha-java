9b122cd3409fa49b4608dabcf8447a65808abd5c
demanxier <demanxier.rodrigues@gmail.com>
2024-07-23 23:10:11 -0300

    https://github.com/Demanxier/trilha-java.git

--- src/main/java/ContaTerminal.java
+++ src/main/java/ContaTerminal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TODO: Conhecer e importar a classe e Scanner
+        //TODO: https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite o numero da CONTA: ");
@@ -13,11 +13,11 @@ public class ContaTerminal {
             String NomeCliente = scanner.next();
         System.out.println("Digite seu SALDO: ");
              Double Saldo = scanner.nextDouble();;
-        //Exibir as mensagens para o nosso usuario
+
         System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
         +  Agencia + ", conta " + Numero + " e seu saldo " + Saldo + " já está diponivel para saque.");
-        //Obter pela scanner os valores digitados no terminal
+
         scanner.close();
-        //Exibir a mensagem conta criada
+
     }
 }
